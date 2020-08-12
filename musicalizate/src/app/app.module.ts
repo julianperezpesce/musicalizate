59a3772e2f0a6c984e904049963120cdb761e27a
julian <estrotococoa@gmail.com>
2020-08-12 11:35:29 -0300

    httpClientModule imported to app.module and HttpClient inyected to home.component

--- musicalizate/src/app/app.module.ts
+++ musicalizate/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,6 +23,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
