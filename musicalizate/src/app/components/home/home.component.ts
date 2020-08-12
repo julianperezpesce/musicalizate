040f2be535537dc87f1884b4e30428114fdf6c39
julian <estrotococoa@gmail.com>
2020-08-12 12:53:39 -0300

    new spotify service at services/

--- musicalizate/src/app/components/home/home.component.ts
+++ musicalizate/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[] = [];
+  
+  constructor(private http: HttpClient) { 
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe( (resp:any) => {
+        this.countries = resp;
+        
+      })
+  }
 
   ngOnInit(): void {
   }
