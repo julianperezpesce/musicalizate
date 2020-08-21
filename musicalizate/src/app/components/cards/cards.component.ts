ed1c1bd2695b07331587963ae15fdfc6cd066bd4
julian <estrotococoa@gmail.com>
2020-08-21 18:08:44 -0300

    router inyected and nagivate

--- musicalizate/src/app/components/cards/cards.component.ts
+++ musicalizate/src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-cards',
@@ -9,7 +10,7 @@ export class CardsComponent {
 
   @Input() items: any[] = [];
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   artistPage( item: any ) {
 
@@ -20,7 +21,7 @@ export class CardsComponent {
     } else {
       artistId = item.id;
     }
-    console.log(artistId);
+    this.router.navigate(['/artist', artistId]);
     
   }
 
